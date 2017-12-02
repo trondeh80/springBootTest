03e5e83332f7705b75b919f2b44ab89298b1ea7d
trondeh80 <trond.erling@gmail.com>
2017-12-02 16:09:06 +0100

    Added neo4j integration. Added a test-endpoint for testdata in db.

--- springtest/src/main/java/com/bouvet/springtest/RecipeController.java
+++ springtest/src/main/java/com/bouvet/springtest/RecipeController.java
@@ -1,13 +1,23 @@
 package com.bouvet.springtest;
+import org.neo4j.driver.v1.*;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @EnableAutoConfiguration
 public class RecipeController {
+
+    private String uri = "bolt://localhost:7687";
+    private String user = "neo4j";
+    private String password = "password";
+    private Driver driver;
 
     @RequestMapping("/")
     @ResponseBody
@@ -15,8 +25,33 @@ public class RecipeController {
         return "Hello World!";
     }
 
+    @RequestMapping("/recipes")
+    @ResponseBody
+    ArrayList<Map> listRecipes(){
+        return getRecipes();
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(RecipeController.class, args);
+    }
+
+    private  ArrayList<Map> getRecipes(){
+        try (Session session = getDriver().session()){
+            StatementResult sr = session.run("MATCH (n:Recipe) RETURN n.id as id, n.name as name LIMIT 28");
+            ArrayList<Map> list = new ArrayList<Map>();
+            while (sr.hasNext()){
+                Record r = sr.next();
+                list.add(r.asMap());
+            }
+            return list;
+        }
+    }
+
+    private Driver getDriver(){
+        if (driver == null) {
+            driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+        }
+        return driver;
     }
 
 }
